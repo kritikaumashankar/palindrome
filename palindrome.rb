1bad4e86dec856a58712c1658fecb09077e182dc
Kritika Umashankar <kritikaumashankar@gmail.com>
2018-08-24 10:39:50 -0600

    changed line 13,14 and 17

--- palindrome.rb
+++ palindrome.rb
@@ -10,13 +10,14 @@ class Palindrome
   def palindrome_method
     new_palin_str = ''
     puts "Enter the String:"
-    @palin_str = gets.chomp
-    len = (@palin_str.length) - 1
+    @palin_str = gets.strip
+    str = @palin_str.gsub(" ","")
+    len = (str.length) - 1
     while len >=0 
-      new_palin_str += @palin_str[len]
+      new_palin_str += str[len]
       len-=1
     end
-    if new_palin_str.casecmp(@palin_str) == 0
+    if new_palin_str.casecmp(str) == 0
       puts "#{@palin_str} is a palindrome!!".colorize(:green)
     else
       puts "#{@palin_str} is not palindrome!!".colorize(:red)
